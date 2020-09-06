681534df0210cce388bdad62341661de95aa0c76
roberto.avila <roberto.avila@UANLMEX-A077.uanlmexico.mx>
2020-09-05 22:19:51 -0500

    Componente de respuesta:    Respuesta de roles.

---
+++ sat/src/main/java/Respuesta/RolPag.java
@@ -0,0 +1,48 @@
+package Respuesta;
+
+import java.util.List;
+
+import com.mtto.sat.modelo.Role;
+
+public class RolPag {
+
+	int page;
+	int perPage;
+	int total;
+	int totalPages;
+	private List<Role> roles;
+	
+	
+	public int getPage() {
+		return page;
+	}
+	public void setPage(int page) {
+		this.page = page;
+	}
+	public int getPerPage() {
+		return perPage;
+	}
+	public void setPerPage(int perPage) {
+		this.perPage = perPage;
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	public int getTotalPages() {
+		return totalPages;
+	}
+	public void setTotalPages(int totalPages) {
+		this.totalPages = totalPages;
+	}
+	public List<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+
+	
+}
